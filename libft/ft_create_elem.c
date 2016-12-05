3adeba627c058894d13f6e4a42f09f4cf8606411
Joanna Jia <jjia25@gmail.com>
2016-12-04 16:57:35 -0800

    does this work now?

---
+++ libft/ft_create_elem.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+t_list *ft_create_elem(char *data)
+{
+    t_list *tmp;
+
+    if(!(tmp = (t_list*)malloc(sizeof(t_list))))
+        return (0);
+    tmp->data = data;
+    tmp->next = NULL;
+    return (tmp);
+}
---
+++ libft/ft_list_size.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int ft_list_size(t_list *begin_list)
+{
+    int count;
+    t_list *temp;
+
+    temp = begin_list;
+    count = 0;
+    while (temp)
+    {
+		count++;
+        temp = temp->next;
+    }
+    return (count);
+}
